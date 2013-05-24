ea6575db1cde0eb537ec81e63253441bf934f369
Steve Tooke <steve@heavi.es>
2013-05-24 12:47:49 +0100

    Create sinatra app

--- generate.rb
+++ generate.rb
@@ -1,31 +1,39 @@
+require 'sinatra'
+require 'haml'
 require 'zipruby'
-require 'pp'
 require 'json'
 require 'active_support/core_ext/string'
 
-zipfile = "/Users/steve/Box Documents/Methods Stethoscope/RCS_Dashboard_Data_Files.zip"
-
-manifest = {}
-
-Zip::Archive.open(zipfile) do |ar|
-  ar.each do |f|
-    next unless f.name =~ /\.txt$/
-    path_elems = f.name.split('/')
-    name_elems = path_elems.last.gsub(/\.txt$/, '').split('_')
-
-    ccg = name_elems[0].gsub("+"," ").titleize
-    ccg.gsub!("Nhs","NHS")
-    ccg.gsub!("Ccg","CCG")
-
-    report_type = path_elems[0]
-
-    report_name = name_elems[1].gsub("+"," ").titleize
-    report_name.gsub!("Ent","ENT")
-
-    manifest[ccg] ||= {}
-    manifest[ccg][report_type] ||= {}
-    manifest[ccg][report_type][report_name] = f.name
-  end
+get "/" do
+  haml :upload
 end
 
-puts manifest.to_json
+post "/" do
+  zipfile = params['myfile'][:tempfile].path
+
+  manifest = {}
+  Zip::Archive.open(zipfile) do |ar|
+    ar.each do |f|
+      next unless f.name =~ /\.txt$/
+      path_elems = f.name.split('/')
+      name_elems = path_elems.last.gsub(/\.txt$/, '').split('_')
+
+      ccg = name_elems[0].gsub("+"," ").titleize
+      ccg.gsub!("Nhs","NHS")
+      ccg.gsub!("Ccg","CCG")
+
+      report_type = path_elems[0]
+
+      report_name = name_elems[1].gsub("+"," ").titleize
+      report_name.gsub!("Ent","ENT")
+
+      manifest[ccg] ||= {}
+      manifest[ccg][report_type] ||= {}
+      manifest[ccg][report_type][report_name] = f.name
+    end
+  end
+
+  attachment "manifest.json"
+  content_type :json
+  manifest.to_json
+end
